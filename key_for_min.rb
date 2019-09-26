c8c7b00afe36823f360504520f5e8ad83ca3c08d
generous-vector-9581 <ojappah@gmail.com>
2019-09-26 22:17:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,12 @@ def key_for_min_value(name_hash)
    min_key = nil
    min_value = nil
    name_hash.each do |key, value|
-
+      if min_value == nil || value < min_value
+         min_value = value
+         min_key = key
+      end
+   end
+   min_key
+end
 
 end
